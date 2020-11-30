117c82f19be6e636fb1945d92dc4eba81d0c5921
dayneglen <dayneglen@gmail.com>
2020-11-30 13:49:31 -0700

    added server info to host

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express'),
+      path = require('path'),
       contactCtrl = require('./contactController'),
       { SERVER_PORT } = process.env;
 
@@ -9,6 +10,12 @@ app.use(express.json());
 
 //Nodemailer endpoint
 app.post('/api/send-email', contactCtrl.sendContact);
+
+app.use(express.static(__dirname + '/../build'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 
 app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}`));
 
